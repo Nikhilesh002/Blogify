6a9eeaff4ad182631ee49e671fd7dd5bc09f0bb1
Nikhilesh <nikhileshg02@gmail.com>
2024-02-17 09:21:52 +0530

    Fixed react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import App from './App.jsx';
+import './index.css';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 import AddPost from './pages/AddPost.jsx';
 import Signup from './pages/Signup.jsx';
@@ -15,72 +15,23 @@ import { AuthLayout } from './components/index.js';
 import EditPost from './pages/EditPost.jsx';
 import Post from './pages/Post.jsx';
 
-
-
-const router=createBrowserRouter([
-  {
-    path:'/',
-    element:<App/>,
-    children:[
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/login",
-        element: (
-          <AuthLayout authentication={false}>
-            <Login />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/signup",
-        element: (
-          <AuthLayout authentication={false}>
-            <Signup />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/all-posts",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <AllPosts />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/add-post",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <AddPost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/edit-post/:slug",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <EditPost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/post/:slug",
-        element: <Post />,
-      }
-    ]
-  }
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}/>
+      <Router basename="/Blogify">
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<AuthLayout authentication={false}><Login /></AuthLayout>} />
+            <Route path="/signup" element={<AuthLayout authentication={false}><Signup /></AuthLayout>} />
+            <Route path="/all-posts" element={<AuthLayout authentication><AllPosts /></AuthLayout>} />
+            <Route path="/add-post" element={<AuthLayout authentication><AddPost /></AuthLayout>} />
+            <Route path="/edit-post/:slug" element={<AuthLayout authentication><EditPost /></AuthLayout>} />
+            <Route path="/post/:slug" element={<Post />} />
+            <Route path="*" element={<div className='text-4xl text-center my-64'>404 Error</div>} />
+          </Route>
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>,
-)
+);
